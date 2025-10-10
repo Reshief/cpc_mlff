cc0e9ce22460d073a47b0a552a59987712efa5b9
Kevin Höllring <kevin.hoellring@fau.de>
2025-10-10 18:19:17 +0200

    Fix loading of test data

--- tests/test_data/load.py
+++ tests/test_data/load.py
@@ -4,13 +4,15 @@ from pathlib import Path
 
 def load_data(filename):
     p_filename = Path(filename)
-    if p_filename.suffix == '.npz':
+    if p_filename.suffix == ".npz":
         import numpy as np
+
         my_resources = importlib.resources.files(__name__)
-        with my_resources.joinpath(filename).open() as stream:
-            return np.load(stream)
+        with my_resources.joinpath(filename).open("rb") as stream:
+            return np.load(stream, allow_pickle=True)
     else:
         from ase.io import iread
+
         my_resources = importlib.resources.files(__name__)
         with my_resources.joinpath(filename).open() as f:
-            return iread(f, ':')
+            return iread(f, ":")
